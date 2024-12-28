7f65b730bceb6fa1e07a7a1299403a7826a68e62
hunsang <hoonsang99@korea.ac.kr>
2024-12-29 02:30:14 +0900

    real fin
    Signed-off-by: hunsang <hoonsang99@korea.ac.kr>

--- run.sh
+++ run.sh
@@ -8,9 +8,10 @@ $SHELL_FOLDER/output/qemu/bin/qemu-system-riscv64 \
 -drive if=pflash,bus=0,unit=0,format=raw,file=$SHELL_FOLDER/output/fw/fw.bin \
 -drive file=$SHELL_FOLDER/output/rootfs/rootfs.img,format=raw,id=hd0 \
 -device virtio-blk-device,drive=hd0 \
+-global virtio-mmio.force-legacy=false \
+-device virtio-gpu-device,id=video0,xres=1280,yres=720 \
 -fw_cfg name="opt/qemu_cmdline",string="qemu_vc="$DEFAULT_V"" \
 -serial vc:$DEFAULT_VC --serial vc:$DEFAULT_VC --serial vc:$DEFAULT_VC --parallel none --monitor vc:$DEFAULT_VC \
--S -s
 #-nographic
 
 #-device virtio-gpu-device,id=video0,xres=1280,yres=720 \
